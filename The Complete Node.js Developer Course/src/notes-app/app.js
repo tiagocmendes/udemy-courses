61b66d268787ee6d72373d9a9e107129cbf1fb17
tiagocmendes <tiagomendes@protonmail.com>
2021-01-28 22:37:03 +0000

    Project setup

---
+++ The Complete Node.js Developer Course/src/notes-app/app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+try {
+    fs.appendFileSync('logging.txt', `${Date.now()} - logging...\n`);
+} catch(err) {
+    console.error(err);
+    fs.writeFileSync('logging.txt', `${Date.now()} - logging...\n`);
+}
