d4c33d6ddca00e75869d7105b0c3d9d296b495db
tiagocmendes <tiagomendes@protonmail.com>
2021-02-02 14:12:47 +0000

    Checking signature

--- The Complete Node.js Developer Course/src/notes-app/app.js
+++ The Complete Node.js Developer Course/src/notes-app/app.js
@@ -1,6 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(`${chalk.green.underline.inverse.bold('Success:')} ${chalk.default.blue(msg)}`);
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note...');
+    }
+});
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing a note...');
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => {
+        console.log('Listing all notes...');
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading a note...');
+    }
+})
+
+// Add, remove, read, list
+console.log(yargs.argv);
